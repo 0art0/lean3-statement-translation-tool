eefe7c0108a75c22ec324a953d99de7924439566
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-16 21:00:03 +0530

    Reorder macros and remove `#` prefix

--- src/interface.lean
+++ src/interface.lean
@@ -3,12 +3,27 @@ import prompting
 open interactive lean.parser
 
 /--!
-Translates a statement to Lean code automatically using OpenAI Codex.
+Provides an interface to the `translate ` and `translate/` commands for automatic translation of mathematical statements to Lean code.
 
-This command is not meant to be used directly, but rather through the `#translate?` command.
+The two-step interface is to avoid triggering the expensive tactics at every keystroke.
 -/
 @[user_command]
-meta def translate_cmd (_ : parse $ tk "#translate ") : lean.parser unit := do
+meta def translate_help (_ : parse $ tk "translate?") : lean.parser unit := do
+  trace "This tool uses OpenAI Codex to turn mathematical statements in natural language to Lean code.\n\n" pure (),
+  stmt ← lean.parser.pexpr,
+  let s := to_string stmt,
+  trace sformat!"To start translating the sentence {s} into a Lean theorem," pure (),
+  suggest_string $ "translate! " ++ s,
+  trace sformat!"\nTo start translating the sentence {s} into a Lean theorem with a docstring," pure (),
+  suggest_string $ "translate/ " ++ s
+
+/--!
+Translates a statement to Lean code automatically using OpenAI Codex.
+
+This command is not meant to be used directly, but rather through the `translate?` command.
+-/
+@[user_command]
+meta def translate_cmd (_ : parse $ tk "translate!") : lean.parser unit := do
   s ← lean.parser.pexpr,
   let stmt := to_string s,
   trace sformat!"Translating {stmt} to Lean code ...\n" pure (),
@@ -19,10 +34,10 @@ meta def translate_cmd (_ : parse $ tk "#translate ") : lean.parser unit := do
 /--!
 Translates a statement to Lean code **with documentation** automatically using OpenAI Codex.
 
-This command is not meant to be used directly, but rather through the `#translate?` command.
+This command is not meant to be used directly, but rather through the `translate?` command.
 -/
 @[user_command]
-meta def translate_with_docstring_cmd (_ : parse $ tk "#translate/") : lean.parser unit := do
+meta def translate_with_docstring_cmd (_ : parse $ tk "translate/") : lean.parser unit := do
   s ← lean.parser.pexpr,
   let stmt := to_string s,
   trace sformat!"Translating {stmt} to Lean code ...\n" pure (),
@@ -31,28 +46,13 @@ meta def translate_with_docstring_cmd (_ : parse $ tk "#translate/") : lean.pars
   suggest_strings $ translations.map (λ t, sformat!"/-- {stmt} -/ {t} :=\n  sorry")
 
 /--!
-Provides an interface to the `#translate ` and `#translate/` commands for automatic translation of mathematical statements to Lean code.
-
-The two-step interface is to avoid triggering the expensive tactics at every keystroke.
+A hole command that invokes the `translate` command to automatically translate mathematical statements to Lean code.
 -/
-@[user_command]
-meta def translate_help (_ : parse $ tk "translate?") : lean.parser unit := do
-  trace "This tool uses OpenAI Codex to turn mathematical statements in natural language to Lean code.\n\n" pure (),
-  stmt ← lean.parser.pexpr,
-  let s := to_string stmt,
-  trace sformat!"To start translating the sentence {s} into a Lean theorem," pure (),
-  suggest_string $ "translate " ++ s,
-  trace sformat!"\nTo start translating the sentence {s} into a Lean theorem with a docstring," pure (),
-  suggest_string $ "translate/ " ++ s
-
-/--!
-A hole command that invokes the `#translate` command to automatically translate mathematical statements to Lean code.
--/
-@[hole_command] meta def translate : hole_command := {
+@[hole_command] meta def translate_hole_cmd : hole_command := {
   name := "translate",
   descr := "Autoformalise to Lean code",
   action := λ ps, do
     [p] ← return ps | tactic.fail "Infer command failed, the hole must contain a single term",
     let s := to_string p,
-    return [("#translate " ++ s, "Translation of " ++ s), ("translate/ " ++ s, "Translation of " ++ s ++ " with docstring")]
+    return [("translate " ++ s, "Translation of " ++ s), ("translate/ " ++ s, "Translation of " ++ s ++ " with docstring")]
   }
