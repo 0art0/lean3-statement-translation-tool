46be0827e51bafc196dc929d15ee13c519aab323
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-26 17:22:19 +0530

    Update demo

--- src/experiments.lean
+++ src/experiments.lean
@@ -1,19 +1,3 @@
 import interface
 
--- UI Demo
-
-section demo
--- translate! "There are infinitely many odd numbers."
-
-/-- There are infinitely many odd numbers. -/
-theorem inf_odds : ∀ n : ℕ, ∃ m : ℕ, m > n ∧ m % 2 = 1 := sorry
-
-/-- A prime is a number that is divisible only by `1` and itself. -/
-def my_prime_number (p : ℕ) := ∀ n : ℕ, p % n = 0 → n = 1 ∨ n = p
-
-/-- `2` is a prime number. -/
-theorem two_prime : my_prime_number 2 := sorry
-
-translate? "There are infinitely many primes that are one greater than a multiple of four."
-
-end demo
+translate? "Every natural number is greater than or equal to zero."
