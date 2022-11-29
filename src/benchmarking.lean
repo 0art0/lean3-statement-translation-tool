1cb7e2c2dcc57d94376421d0b2e4b9d3865a0d80
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-29 15:52:09 +0530

    Add code for benchmarking

--- src/benchmarking.lean
+++ src/benchmarking.lean
@@ -30,19 +30,30 @@ meta def translation_summary (stmt : string) (record_params := ff)
       ]
 
 /-- Reads in `input_file` (**without** `.json` suffix) as a list of strings,
-    translates each string and summarises the results, and finally writes the results back to a `.json` file. -/
-meta def main (input_file : string)
-    (use_fixed := tt)
-    (n_sim := 15) 
-    (temp := 6) 
-    (n := 10) 
-    (completion_prefix := "theorem") : io unit := do
+    translates each string and summarises the results, and finally writes the results back to a `.json` file. 
+    
+    To actually execute this, first set the `input_file_name`, adjust the parameters and then 
+    run `lean --run src/benchmarking.lean` from the outer directory of the repository. -/
+meta def main : io unit := do
+  -- the parameters for translation
+  let use_fixed := tt,
+  let n_sim := 15, 
+  let temp := 6, 
+  let n := 10, 
+  let completion_prefix := "theorem",
+
+  -- the input file
+  let input_file_name := "data/test_statements",
+  io.print_ln sformat! "Processing {input_file_name}.json ...",
   -- the input file as a `json` object
-  input ← io.fs.read_file (input_file ++ ".json") >>= pure ∘ json.parse ∘ buffer.to_string,
+  input ← io.fs.read_file (input_file_name ++ ".json") >>= pure ∘ json.parse ∘ buffer.to_string,
   stmts ← io.of_except (do
     data ← except.of_option input "failed to parse input file as json",
     data_arr ← data.as_array,
     data_arr.mmap json.as_string),
+  io.print_ln "Translating statements...",
   output ← stmts.mmap $ λ s, translation_summary s ff use_fixed n_sim ff temp n completion_prefix,
-  output_file ← io.mk_file_handle (sformat! "{input_file}-results-F{use_fixed}-S{n_sim}-T{temp}-N{n}.json") io.mode.write,
+  let output_file_name := sformat! "{input_file_name}-results-F{use_fixed}-S{n_sim}-T{temp}-N{n}.json",
+  io.print_ln sformat!"Writing output to {output_file_name}...",
+  output_file ← io.mk_file_handle output_file_name io.mode.write,
   io.fs.write output_file $ (json.unparse output).to_char_buffer
