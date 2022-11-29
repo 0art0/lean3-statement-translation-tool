30dbc9bc0811bce92700e47e722f60dbe18d8762
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-29 13:06:44 +0530

    More docs for interface

--- src/interface.lean
+++ src/interface.lean
@@ -3,7 +3,7 @@ import prompting
 open interactive lean.parser
 
 /--!
-Provides an interface to the `translate ` and `translate/` commands for automatic translation of mathematical statements to Lean code.
+Provides an interface to the `translate! ` command for automatic translation of mathematical statements to Lean code.
 
 The two-step interface is to avoid triggering the expensive tactics at every keystroke.
 -/
@@ -17,6 +17,7 @@ meta def translate_help (_ : parse $ tk "translate?") : lean.parser unit := do
   trace sformat!"\nTo start translating the sentence {s} into a Lean definition (not implemented)," pure (),
   suggest_string $ "translate_def! " ++ s
 
+/-- The core function responsible for reading the string, creating the prompt, getting and processing the completions from Codex and displaying them in the infoview. -/
 meta def translate_core 
   (use_fixed := tt) -- whether to use fixed prompts
   (n_sim := 15) -- the number of prompts retrieved from `mathlib` by sentence similarity
@@ -43,6 +44,11 @@ This command is not meant to be used directly, but rather through the `translate
 meta def translate_cmd (_ : parse $ tk "translate!") : lean.parser unit :=
   translate_core
 
+/--!
+Translates a statement to Lean code automatically using OpenAI Codex with **only fixed prompts**.
+
+This command is not meant to be used directly, but rather through the `translate?` command.
+-/
 @[user_command]
 meta def translate_fixed_cmd (_ : parse $ tk "translate₀") : lean.parser unit :=
   translate_core tt 0
