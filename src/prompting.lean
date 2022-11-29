1981299b209e87adb679f36d749e6f4a3ecb9090
ART0 <18333981+0Art0@users.noreply.github.com>
2022-11-29 15:36:02 +0530

    Return `prompt` in `get_translations`

--- src/prompting.lean
+++ src/prompting.lean
@@ -22,7 +22,7 @@ meta def get_translations (stmt : string)
     (use_ctx := tt) 
     (temp := 6) 
     (n := 7) 
-    (prompt_suffix := "theorem") : io (list string) := do
+    (prompt_suffix := "theorem") : io (string × list string) := do
   let fix_prompts := if use_fixed then fixed_prompts else [],
   sim_prompts ← similar_prompts stmt n_sim,
   ctx_prompts ← if use_ctx then context_prompts else pure [],
@@ -30,7 +30,7 @@ meta def get_translations (stmt : string)
   let main_prompt := (build_prompt all_prompts) ++ sformat!"/-- {stmt} -/\n" ++ prompt_suffix,
   
   translations ← completion_request.get_codex_completions {prompt := main_prompt, temperature := temp, n := n},
-  return $ translations.map (λ t, prompt_suffix ++ t)
+  return $ (main_prompt, translations.map (λ t, prompt_suffix ++ t))
 
 /-- Post-process the Codex completions by converting to `declaration_with_docstring` and typechecking. -/
 meta def process_translations (stmt : string)
@@ -40,7 +40,7 @@ meta def process_translations (stmt : string)
     (temp := 6) 
     (n := 7) 
     (completion_prefix := "theorem") : tactic (list declaration_with_docstring × list declaration_with_docstring) := do
-  translations ← tactic.unsafe_run_io $ get_translations stmt use_fixed n_sim use_ctx temp n completion_prefix,
+  (_, translations) ← tactic.unsafe_run_io $ get_translations stmt use_fixed n_sim use_ctx temp n completion_prefix,
   let translation_decls := translations.erase_dups.map $ λ t, declaration_with_docstring.from_string t stmt,
   (typecorrect_translations, failed_translations) ← translation_decls.split_with $ 
       (functor.map option.is_some) ∘ declaration_with_docstring.validate,
